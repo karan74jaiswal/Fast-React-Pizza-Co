fd25fd7106c049c7254eeb6236889464a85678e5
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2024-05-11 01:59:19 +0530

    used react router data fetching to fetch some data on page rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import NotFound from "./ui/Error";
 import AppLayout from "./ui/AppLayout";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -18,6 +19,7 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        loader: menuLoader,
       },
       {
         path: "/cart",
